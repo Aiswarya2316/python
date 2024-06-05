1d8c5c52de0e24cbb7d53e0b8b4be3d1b81cae6f
Aiswarya2316 <aiswaryapinkey@gmail.com>
2024-06-05 09:43:08 +0530

    pythom

---
+++ python.py
@@ -0,0 +1,47 @@
+print('welcome')  
+# age=21
+# name='Aiswarya'
+# weight='45'
+
+# print('age is :', age)
+# print('name :',name)
+# print('weight :',weight)
+
+# a=10
+# print(type(a))
+
+# b=20.5
+# print(type(b))
+
+# c=2+5j
+# print(type(c))
+
+# name="manu"
+# print(type(name))
+
+# d=True
+# print(type(d))
+
+# e=False
+# print(type(e))
+
+# l=[10,20,30]
+# print(type(l))
+
+# t=(10,20,30)
+# print(type(t))
+
+# s={10,20,30}
+# print(type(s))
+
+# d={'name':'manu'}
+# print(type(d))
+
+age = int(input('Enter age'))
+print('age is :', age)
+
+name = str(input('Enter name'))
+print('name :',name)
+
+weight = float(input('Enter weight'))
+print('weight :',weight)
