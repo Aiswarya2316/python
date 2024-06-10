1011eb583f3f74bb2a69651d04a6aa7374db346f
Aiswarya2316 <aiswaryapinkey@gmail.com>
2024-06-10 22:32:03 +0530

    python

--- task6.py
+++ task6.py
@@ -1,9 +1,12 @@
 costprice=float(input('enter cost price'))
 if costprice > 100000:
         tax_percentage = 15
+        print("tax:",tax_percentage)
 elif costprice > 50000:
         tax_percentage = 10
+        print("tax:",tax_percentage)
 else:
         tax_percentage = 5
+        print("tax:",tax_percentage)
     
     
