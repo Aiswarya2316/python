317c87c000ff603212a229ea54a66fed5a21ccf0
Aiswarya2316 <aiswaryapinkey@gmail.com>
2024-06-11 11:25:03 +0530

    python

---
+++ task12.py
@@ -0,0 +1,8 @@
+s="Welcome to all"
+f=input("Enter a word or letter ")
+
+if f in s:
+    print("yes")
+
+else:
+    print("no")
---
+++ task9.py
@@ -0,0 +1,18 @@
+a=int(input("Enter first number"))
+b=int(input("Enter second number"))
+c=int(input("Enter third number"))
+
+if a>b:
+    if a>c:
+        print( "greatest number is:",a)
+
+    else:
+        print("greatest number is:",c)
+
+else:
+    if b>c:
+        print("greatest number is:",b)
+
+    else:
+        print("greatest number is:",c)    
+
