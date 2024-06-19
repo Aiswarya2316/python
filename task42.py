08447703dc46cdeaaa516d5a3cb70c136a39a092
Aiswarya2316 <aiswaryapinkey@gmail.com>
2024-06-19 23:13:34 +0530

    python

--- task42.py
+++ task42.py
@@ -1,44 +1,46 @@
-# a=int(input('enter a value'))
-# i=1
-# while i<=10:
-#     print(i,'*',a,'=',a*i)
-#     i+=1
+a=int(input('enter a value'))
+i=1
+while i<=10:
+    print(i,'*',a,'=',a*i)
+    i+=1
 
 
-# a=int(input('enter a number'))
-# rev=0
-# while a>0:
-#     d=a%10
-#     rev=(rev*10)+d
-#     a//=10
-# print(rev)
+a=int(input('enter a number'))
+rev=0
+while a>0:
+    d=a%10
+    rev=(rev*10)+d
+    a//=10
+    if a==0:
+        break
+print(rev)
 
 
-# a=int(input('enter a number'))
-# sum=0
-# while a>0:
-#     d=a%10
-#     a//=10
-#     sum+=d
-#     print(sum)
+a=int(input('enter a number'))
+sum=0
+while a>0:
+    d=a%10
+    a//=10
+    sum+=d
+print(sum)
 
 
-# a='welcome'
-# l=len(a)
-# i=0
-# while i<l:
-#     print(a[i])
-#     i+=1
+a='welcome'
+l=len(a)
+i=0
+while i<l:
+    print(a[i])
+    i+=1
 
 
-# a='welcome'
-# l=len(a)
-# i=0
-# rev=''
-# while i<l:
-#     rev=a[i]+rev
-#     i+=1
-# print(rev)
+a='welcome'
+l=len(a)
+i=0
+rev=''
+while i<l:
+    rev=a[i]+rev
+    i+=1
+print(rev)
 
 
 a=0
