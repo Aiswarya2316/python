c772c7cf174efa232ae70cdca54d0695df575bff
Aiswarya2316 <aiswaryapinkey@gmail.com>
2024-06-20 11:14:31 +0530

    python

--- task45.py
+++ task45.py
@@ -1,22 +1,42 @@
    ## list
 
-L=[1,2,"welcome",7.5,1,2]
-print(L[2])
-if 2 in L:
-    print()
-else:
-    print()
-for i in L:
-    print(i) 
-print (type(L)) 
-print(len(L))          
+# L=[1,2,"welcome",7.5,1,2]
+# print(L[2])
+# if 2 in L:
+#     print()
+# else:
+#     print()
+# for i in L:
+#     print(i) 
+# print (type(L)) 
+# print(len(L))          
 
 
 
-L=[1,2,'welcome',7.5,2,4,6]
-print(L[2])
-L[2]="helloooooo"
-print(L)
+# L=[1,2,'welcome',7.5,2,4,6]
+# print(L[2])
+# L[2]="helloooooo"
+# print(L)
 
-del L[2]
+# del L[2]
+# print(L)
+
+
+
+     #list meathods
+
+# L=[]
+# L.append(20)
+# L.append(25)
+# L.append([30,40,50])     
+# print(L)
+
+
+# L=[5,15]
+# L.insert(2,20)
+# print(L)
+
+
+L=[50,100]
+L.extend([100,200,300])
 print(L)
