b28c3512e944d18f16e2ece163d20ca2b788096f
amalkrishnaan <aiswaryapinkey@gmail.com>
2024-06-22 11:19:33 +0530

    python

--- task50.py
+++ task50.py
@@ -5,6 +5,7 @@
 #     if my_list[i] not in my_list[:i]:
 #         print(my_list[i])
 
+              #or
 
 # original=[1,2,3,2,3]
 # unique=[]
@@ -16,16 +17,35 @@
 
     # print no and str find sum of no
 
+# L=[10,20,30,40,'a','b','c','d'] 
+# print(" list:", L)
+# sum=0
+# for i in  L:
+#    if  type(i) in (int,float):
+#      sum += i
+# print("sum is:",sum)
+
+            #or
+
 L=[10,20,30,40,'a','b','c','d'] 
 print(" list:", L)
 sum=0
 for i in  L:
-   if  type(i) in (int,float):
+   if  type(i)==int or type(i)==float:
      sum += i
 print("sum is:",sum)
 
 
+    # print the largest number in the list
 
+L=[10,20,30,40,50]
+print(L)
+large=max(L)
+print(large)
 
+        #or
 
-
+L=[10,40,30,50,20]
+L.sort()
+print(L[-1])
+print(L)
