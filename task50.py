20584c2847e16fe0abb8004ddd0983772ebbfb64
amalkrishnaan <aiswaryapinkey@gmail.com>
2024-06-22 11:20:34 +0530

    python

--- task50.py
+++ task50.py
@@ -1,29 +1,29 @@
          # print list avoid duplicate
 
-# my_list = [1, 2, 3, 4, 3, 4]
-# for i in range(len(my_list)):
-#     if my_list[i] not in my_list[:i]:
-#         print(my_list[i])
+my_list = [1, 2, 3, 4, 3, 4]
+for i in range(len(my_list)):
+    if my_list[i] not in my_list[:i]:
+        print(my_list[i])
 
               #or
 
-# original=[1,2,3,2,3]
-# unique=[]
-# for item in original:
-#     if item not in unique:
-#         unique.append(item)
-# print(unique)
+original=[1,2,3,2,3]
+unique=[]
+for item in original:
+    if item not in unique:
+        unique.append(item)
+print(unique)
 
 
     # print no and str find sum of no
 
-# L=[10,20,30,40,'a','b','c','d'] 
-# print(" list:", L)
-# sum=0
-# for i in  L:
-#    if  type(i) in (int,float):
-#      sum += i
-# print("sum is:",sum)
+L=[10,20,30,40,'a','b','c','d'] 
+print(" list:", L)
+sum=0
+for i in  L:
+   if  type(i) in (int,float):
+     sum += i
+print("sum is:",sum)
 
             #or
 
