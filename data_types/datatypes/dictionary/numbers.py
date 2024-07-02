2c70a101d921bafcce7a4b89a47b258ff9f15ef1
Aiswarya2316 <aiswaryapinkey@gmail.com>
2024-07-02 09:37:35 +0530

    done

--- data_types/datatypes/dictionary/numbers.py
+++ data_types/datatypes/dictionary/numbers.py
@@ -9,18 +9,18 @@
                #or
 
 
-num = {
-    0: 'zero', 1: 'one', 2: 'two', 3: 'three', 4: 'four',
-    5: 'five', 6: 'six', 7: 'seven', 8: 'eight', 9: 'nine'
-}
+# num = {
+#     0: 'zero', 1: 'one', 2: 'two', 3: 'three', 4: 'four',
+#     5: 'five', 6: 'six', 7: 'seven', 8: 'eight', 9: 'nine'
+# }
 
-a = int(input("Enter a number : "))
+# a = int(input("Enter a number : "))
 
-if 0 <= a <= 9:
-    for i in range(a + 1):  
-        print(num[i])
-else:
-    print("Invalid input.")
+# if 0 <= a <= 9:
+#     for i in range(a + 1):  
+#         print(num[i])
+# else:
+#     print("Invalid input.")
 
 
 
@@ -28,12 +28,12 @@ else:
 
 
 
-# num={0:'zero',1:'one',2:'two',3:'three',4:'four',5:'five',6:'six',7:'seven',8:'eight',9:'nine'}
-# a=int(input("enter the number :"))
-# a_str = str(a)
-# result = ""
-# for char in a_str:
-#     digit = int(char)
-#     if digit in num:
-#         result += num[digit] + " "
-# print(result.strip())                
+num={0:'zero',1:'one',2:'two',3:'three',4:'four',5:'five',6:'six',7:'seven',8:'eight',9:'nine'}
+a=int(input("enter the number :"))
+a_str = str(a)
+result = ""
+for char in a_str:
+    digit = int(char)
+    if digit in num:
+        result += num[digit] + " "
+print(result.strip())                
