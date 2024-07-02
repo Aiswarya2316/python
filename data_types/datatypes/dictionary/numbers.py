a0d31f5250b05ccf3f91fc8d722b218d426703bd
Aiswarya2316 <aiswaryapinkey@gmail.com>
2024-07-02 11:21:19 +0530

    done

--- data_types/datatypes/dictionary/numbers.py
+++ data_types/datatypes/dictionary/numbers.py
@@ -28,12 +28,28 @@
 
 
 
-num={0:'zero',1:'one',2:'two',3:'three',4:'four',5:'five',6:'six',7:'seven',8:'eight',9:'nine'}
-a=int(input("enter the number :"))
-a_str = str(a)
-result = ""
-for char in a_str:
-    digit = int(char)
-    if digit in num:
-        result += num[digit] + " "
-print(result.strip())                
+# num={0:'zero',1:'one',2:'two',3:'three',4:'four',5:'five',6:'six',7:'seven',8:'eight',9:'nine'}
+# a=int(input("enter the number :"))
+# a_str = str(a)
+# result = ""
+# for char in a_str:
+#     digit = int(char)
+#     if digit in num:
+#         result += num[digit] + " "
+# print(result.strip())                
+
+
+                            # or
+
+
+
+number = {0: 'zero', 1: 'one', 2: 'two', 3: 'three', 4: 'four', 5: 'five', 6: 'six', 7: 'seven', 8: 'eight', 9: 'nine'}
+a = int(input('Enter a number: '))
+result = ''
+while a > 0:
+    digit = a % 10  
+    word = number[digit]  
+    result = word + ' ' + result 
+    a = a // 10 
+result = result
+print(result)
---
+++ functions/task1.py
@@ -0,0 +1,6 @@
+print("start")
+def fun1():
+    print("heloo")
+    print("welcome")
+fun1()
+print("stop")
