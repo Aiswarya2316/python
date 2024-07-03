02f0fe4ff6c4f903b8ea1b62280a710310887250
Aiswarya2316 <aiswaryapinkey@gmail.com>
2024-07-03 10:49:40 +0530

    done

--- functions/task1.py
+++ functions/task1.py
@@ -46,8 +46,18 @@
 
             #   type of function
 
-def add(a,b,c):
-    return a+b-c
+# def add(a,b,c):
+#     return a+b-c
 
-print(add(10,20,30))
-print(add(40,50,60))
+# print(add(10,20,30))
+# print(add(40,50,60))
+
+
+
+
+
+def add(name,age=21):
+    return name,age
+
+print(add('abc',20))
+print(add('aisu'))
