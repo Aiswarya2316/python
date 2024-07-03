661c97d357541069b1dee6ba3cf7677ebcc35b4c
Aiswarya2316 <aiswaryapinkey@gmail.com>
2024-07-03 09:58:55 +0530

    done

--- functions/task1.py
+++ functions/task1.py
@@ -1,6 +1,15 @@
-print("start")
-def fun1():
-    print("heloo")
-    print("welcome")
-fun1()
-print("stop")
+# print("start")
+# def fun1():
+#     print("heloo")
+#     print("welcome")
+# fun1()
+# print("stop")
+
+
+
+a=30 #global
+def fun2():
+    a=20  #local
+    print("welcome",a)
+fun2()
+print(a)
