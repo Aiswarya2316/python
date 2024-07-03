24734209b254111231fb20533bd29e1993ff5673
Aiswarya2316 <aiswaryapinkey@gmail.com>
2024-07-03 10:21:55 +0530

    done

--- functions/task1.py
+++ functions/task1.py
@@ -7,9 +7,35 @@
 
 
 
-a=30 #global
-def fun2():
-    a=20  #local
-    print("welcome",a)
-fun2()
-print(a)
+
+
+# a=30 #global
+# def fun2():
+#     a=20  #local
+#     print("welcome",a)
+# fun2()
+# print(a)
+
+
+
+
+
+# def fun3():
+#     global b
+#     b=20
+#     print("global",b)
+# fun3()
+# print(b)    
+
+
+
+
+
+a=20
+def fun4():
+    global b
+    print("global",b)
+    c=30
+    return c,40,50
+c1,d1,f1=fun4()
+print("return",c1,d1,f1)
