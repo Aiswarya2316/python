c3b7ea6fcf5ea12b7fbbb3a456900bbc8e61161e
Aiswarya2316 <aiswaryapinkey@gmailcom>
2024-07-04 10:06:00 +0530

    done

---
+++ functions/arithematic.py
@@ -0,0 +1,34 @@
+
+def add(a,b):
+    return a+b
+def sub(a,b):
+    return a-b
+def pro(a,b):
+    return a*b
+def div(a,b):
+    return a/b
+def num():
+    a=int(input("Enter a number:"))
+    b=int(input("Enter a number:"))
+    return a,b
+
+while True:
+    print("1.add\n2.sub\n3.pro\n4.div")
+    ch=int(input("Enter your choice:"))
+    if ch==1:
+        a,b=num()
+        print(add(a,b))
+    elif ch==2:
+        a,b=num() 
+        print(sub(a,b))
+    elif ch==3:
+        a,b=num()
+        print(pro(a,b))
+    elif ch==4:
+        a,b=num()
+        print(div(a,b))
+    elif ch==5:
+        break    
+
+
+
