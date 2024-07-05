a0453e7656824d1da1247d95f4b432e7861fa64f
Aiswarya2316 <aiswaryapinkey@gmail.com>
2024-07-05 10:47:41 +0530

    done

--- module/module2.py
+++ module/module2.py
@@ -1,25 +1,44 @@
-        #1
+#         #1
 
-import module1
-module1.fun()
-module1.fun1()
-
-
-        #2
-
-import module1 as mod1
-mod1.fun()
-mod1.fun1()
+# import module1
+# module1.fun()
+# module1.fun1()
 
 
-        #3
+#         #2
 
-from module1 import fun,fun1
-fun(),fun1()
+# import module1 as mod1
+# mod1.fun()
+# mod1.fun1()
 
 
-        #4
+#         #3
 
-from module1 import*
-fun()
-fun1()
+# from module1 import fun,fun1
+# fun(),fun1()
+
+
+#         #4
+
+# from module1 import*
+# fun()
+# fun1()
+
+
+from module3 import*
+# print(add(10,20,30))
+# print(add(40,50,60))
+
+
+# print(add('abc',20))
+# print(add('aisu'))
+
+# add("ammu",21)    
+# add(name="achu",age=21)
+
+
+# print (add("achu","ammu","anu","devu","malu"))
+# print (add())
+
+print(add(name="ammu",age=21,place="ekm",state="kerala",capital="trivandram"))
+
---
+++ module/module3.py
@@ -0,0 +1,39 @@
+# def add(a,b,c):
+#     return a+b-c
+
+
+
+
+
+
+
+# def add(name,age=21):
+#     return name,age
+
+
+
+
+
+
+
+# def add(name,age):
+#     print("name:",name)
+#     print("age:",age)
+
+
+
+
+
+
+
+
+# def add(*a):
+#     return(a)
+
+
+
+
+
+
+def add(**a):
+    return(a)
