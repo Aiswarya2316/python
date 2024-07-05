2b8f5fbb7d2f7d5babc421a4b77e6691bb949e27
Aiswarya2316 <aiswaryapinkey@gmail.com>
2024-07-05 11:30:54 +0530

    done

--- module/tsk1/main.py
+++ module/tsk1/main.py
@@ -1,7 +1,10 @@
-from python.module.tsk1.reg import *
+from reg import *
+from dis import *
 data=[]
 while True:
     print("1.add\n2.display\n3.update\n4.dalete\n5.Exit")
     ch=int(input("Enter your choice:"))
     if ch==1:
         add(data)
+    elif ch==2:
+        dis(data)    
--- module/tsk1/reg.py
+++ module/tsk1/reg.py
@@ -3,4 +3,4 @@ def add(data):
     id=int(input("Enter id:"))
     age=int(input("Enter your age:"))
     place=str(input("Enter ur place:"))
-    data.append("name":name,"id":id,"age":age,"place":place)
+    data.append({"name":name,"id":id,"age":age,"place":place})
