427d16524e3f2e221cd295611c5a6481b21bcb1b
Aiswarya2316 <aiswaryapinkey@gmail.com>
2024-07-05 10:12:10 +0530

    done

--- module/module2.py
+++ module/module2.py
@@ -1,16 +1,24 @@
+        #1
+
 import module1
 module1.fun()
 module1.fun1()
 
+
+        #2
 
 import module1 as mod1
 mod1.fun()
 mod1.fun1()
 
 
+        #3
+
 from module1 import fun,fun1
 fun(),fun1()
 
+
+        #4
 
 from module1 import*
 fun()
