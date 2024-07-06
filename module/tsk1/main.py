d311e77234365ca196ce65d02cac4f375e71ec6d
Aiswarya2316 <aiswaryapinkey@gmail.com>
2024-07-06 10:41:05 +0530

    done

--- module/tsk1/main.py
+++ module/tsk1/main.py
@@ -1,7 +1,9 @@
 from reg import *
 from dis import *
 from upd import *
-data=[]
+from delet import *
+data=[{"id": "1", "name": "Alice", "age": "30", "place": "New York"},
+    {"id": "2", "name": "Bob", "age": "25", "place": "Chicago"},]
 while True:
     print("1.add\n2.display\n3.update\n4.dalete\n5.Exit")
     ch=int(input("Enter your choice:"))
@@ -9,7 +11,12 @@ while True:
         add(data)
     elif ch==2:
         dis(data) 
-
     elif ch == 3:
         up(data)
-       
+    elif ch == 4:
+        dele(data)
+    elif ch == 5:
+        print("You had exited")
+        break
+    else:
+        ("Invalid input")
