9d3d683403eada1973934b0cc006b3344ada08e9
Aiswarya2316 <aiswaryapinkey@gmail.com>
2024-07-08 11:06:47 +0530

    done

--- module/filehandling/file_handling_functions.py
+++ module/filehandling/file_handling_functions.py
@@ -1,5 +1,9 @@
 # f=open("file.txt","x")
 # f.write("123"+"abcdefghijklmnopqrstuvwxyz")
 
-f=open("python/module/filehandling/file1.txt","x")
-f.write("123"+"abcdefghijklmnopqrstuvwxyz")
+# f=open("python/module/filehandling/file1.txt","x")
+# f.write("123"+"abcdefghijklmnopqrstuvwxyz")
+
+
+f=open("python/module/filehandling/file3.txt","w")
+f.write("abcdefghijklmnopqrstuvwxyz")
