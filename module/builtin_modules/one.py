8463b94a0cb498bd30871e43dd861be196a146c4
Aiswarya2316 <aiswaryapinkey@gmail.com>
2024-07-08 10:35:07 +0530

    done

---
+++ module/builtin_modules/one.py
@@ -0,0 +1,6 @@
+import math
+print(math.factorial(5))
+print(math.floor(5.8))
+print(math.ceil(5.8))
+print(math.sqrt(64))
+print(math.pow(4,2))
---
+++ module/builtin_modules/two.py
@@ -0,0 +1,6 @@
+import datetime
+print(datetime.datetime.now())
+print(datetime.datetime.now().year)
+print(datetime.datetime.now().strftime("%A"))
+print(datetime.datetime.now().strftime("%p"))
+print(datetime.datetime.now().strftime("%X"))
