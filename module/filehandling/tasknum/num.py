b3d76c0d04f473a8d9ebb863af843a2846e4ade2
Aiswarya2316 <aiswaryapinkey@gmail.com>
2024-07-10 11:13:54 +0530

    done

--- module/filehandling/tasknum/num.py
+++ module/filehandling/tasknum/num.py
@@ -1,16 +1,5 @@
-f=open("python/module/filehandling/tasknum/mul.txt","x")
-def print_multiplication_table(number, upto=10):
-    """
-    Prints the multiplication table of the given number up to the specified limit.
-    
-    Parameters:
-    - number (int): The number whose multiplication table is to be printed.
-    - upto (int): Optional. The upper limit for the multiplication table. Default is 10.
-    """
-    print(f"Multiplication table of {number}:")
-    for i in range(1, upto + 1):
-        print(f"{number} x {i} = {number * i}")
-
-if __name__ == "__main__":
-    num = int(input("Enter a number: "))
-    print_multiplication_table(num)
+f = open("python/module/filehandling/tasknum/table.txt", "w")
+a = int(input("Enter the number: "))
+for i in range(1, 11):
+    b = str(i) + " * " + str(a) + " = " + str(i * a) + "\n"
+    f.write(b)
