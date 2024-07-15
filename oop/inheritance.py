8d7c35e7e1e75eb054b56a1007620f167d31934e
Aiswarya2316 <aiswaryapinkey@gmail.com>
2024-07-15 10:04:40 +0530

    done

---
+++ oop/inheritance.py
@@ -0,0 +1,24 @@
+        #single
+
+class synnefo:
+    def __init__(s):
+        print('register')
+    def python(s):
+        s.database='mysql'
+        print("python")
+    def php(self):
+        self.database='mysql'
+        print("php",self.database)
+
+class novavi(synnefo):
+    def dm(self):
+        print("digital marketing")
+    def web_dev(self):
+        print("web developer")
+
+anu=synnefo()
+anu.php()
+
+achu=novavi()
+achu.dm()
+achu.python()
