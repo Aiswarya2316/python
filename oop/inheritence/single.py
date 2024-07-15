08521f6e94384abde08c722626150d639a222c5b
Aiswarya2316 <aiswaryapinkey@gmail.com>
2024-07-15 11:15:36 +0530

    done

--- oop/inheritence/single.py
+++ oop/inheritence/single.py
@@ -1,27 +1,27 @@
         #single
 
-# class synnefo:
-#     def __init__(s):
-#         print('register')
-#     def python(s):
-#         s.database='mysql'
-#         print("python")
-#     def php(self):
-#         self.database='mysql'
-#         print("php",self.database)
+class synnefo:
+    def __init__(s):
+        print('register')
+    def python(s):
+        s.database='mysql'
+        print("python")
+    def php(self):
+        self.database='mysql'
+        print("php",self.database)
 
-# class novavi(synnefo):
-#     def dm(self):
-#         print("digital marketing")
-#     def web_dev(self):
-#         print("web developer")
+class novavi(synnefo):
+    def dm(self):
+        print("digital marketing")
+    def web_dev(self):
+        print("web developer")
 
-# anu=synnefo()
-# anu.php()
+anu=synnefo()
+anu.php()
 
-# achu=novavi()
-# achu.dm()
-# achu.python()
+achu=novavi()
+achu.dm()
+achu.python()
 
          
          
---
+++ oop/inheritence/task_inheritence.py
@@ -0,0 +1,39 @@
+                #single inheritance
+class college:
+    def __init__(s):
+        print('register')
+    def python(s):
+        print("python")
+    def php(self):
+        print("php")
+    def english(self):
+        print("fine tune") 
+    def accounting(self):
+        print("bank accounting") 
+    def span(self):
+        print("spanish")            
+
+class teachers(college):
+    def web_dev(self):
+        print("web developer")
+    def maths(self):
+        print("mathematics")    
+
+anu=college()
+anu.accounting
+
+achu=teachers()
+achu.maths
+achu.english
+
+
+
+
+
+                #multiple inheritance
+                
+                         
+         
+
+           
+           
