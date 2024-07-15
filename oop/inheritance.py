e6a083123ab94c193ba96c6e21ac5c083a6b89f3
Aiswarya2316 <aiswaryapinkey@gmail.com>
2024-07-15 10:18:20 +0530

    done

--- oop/inheritance.py
+++ oop/inheritance.py
@@ -1,5 +1,30 @@
         #single
 
+# class synnefo:
+#     def __init__(s):
+#         print('register')
+#     def python(s):
+#         s.database='mysql'
+#         print("python")
+#     def php(self):
+#         self.database='mysql'
+#         print("php",self.database)
+
+# class novavi(synnefo):
+#     def dm(self):
+#         print("digital marketing")
+#     def web_dev(self):
+#         print("web developer")
+
+# anu=synnefo()
+# anu.php()
+
+# achu=novavi()
+# achu.dm()
+# achu.python()
+
+            #multiple
+
 class synnefo:
     def __init__(s):
         print('register')
@@ -10,15 +35,23 @@ class synnefo:
         self.database='mysql'
         print("php",self.database)
 
-class novavi(synnefo):
+class novavi:
     def dm(self):
         print("digital marketing")
     def web_dev(self):
         print("web developer")
+
+class std(novavi,synnefo):
+    def reg(self):
+        print('std reg')
 
 anu=synnefo()
 anu.php()
 
 achu=novavi()
 achu.dm()
-achu.python()
+
+anju=std()
+anju.python()
+
+
