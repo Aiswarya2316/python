31195b644fa00cc5dc80fa9e032f6df5aba01ebe
Aiswarya2316 <aiswaryapinkey@gmail.com>
2024-07-15 11:06:25 +0530

    done

---
+++ oop/inheritence/heirarchial.py
@@ -0,0 +1,34 @@
+ #heirarchichal
+
+
+class synnefo:
+    def __init__(s):
+        print('register')
+    def python(s):
+        s.database='mysql'
+        print("python")
+    def php(self):
+        self.database='mysql'
+        print("php",self.database)
+
+class novavi(synnefo):
+    def dm(self):
+        print("digital marketing")
+    def web_dev(self):
+        print("web developer")
+
+class std(synnefo):
+    def reg(self):
+        print('std reg')
+
+anu=synnefo()
+anu.php()
+
+achu=novavi()
+achu.dm()
+achu.database
+
+anju=std()
+anju.python()
+anju.reg
+
