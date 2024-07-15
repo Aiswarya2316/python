4bb74d2367a8661627261c580987881380984a71
Aiswarya2316 <aiswaryapinkey@gmail.com>
2024-07-15 11:21:47 +0530

    done

--- oop/inheritence/task_inheritence.py
+++ oop/inheritence/task_inheritence.py
@@ -1,4 +1,37 @@
                 #single inheritance
+# class college:
+#     def __init__(s):
+#         print('register')
+#     def python(s):
+#         print("python")
+#     def php(self):
+#         print("php")
+#     def english(self):
+#         print("fine tune") 
+#     def accounting(self):
+#         print("bank accounting") 
+#     def span(self):
+#         print("spanish")            
+
+# class teachers(college):
+#     def web_dev(self):
+#         print("web developer")
+#     def maths(self):
+#         print("mathematics")    
+
+# anu=college()
+# anu.accounting
+
+# achu=teachers()
+# achu.maths
+# achu.english
+
+
+
+
+
+                #multiple inheritance
+
 class college:
     def __init__(s):
         print('register')
@@ -11,28 +44,27 @@ class college:
     def accounting(self):
         print("bank accounting") 
     def span(self):
-        print("spanish")            
+        print("spanish") 
 
-class teachers(college):
+class teachers:
     def web_dev(self):
         print("web developer")
     def maths(self):
-        print("mathematics")    
+        print("mathematics") 
+
+class students(teachers,college):
+    def register(self):
+        print('std reg')
 
 anu=college()
-anu.accounting
+anu.php()
 
 achu=teachers()
-achu.maths
-achu.english
+achu.web_dev()
 
+anju=students()
+anju.python()                
 
-
-
-
-                #multiple inheritance
-                
-                         
          
 
            
