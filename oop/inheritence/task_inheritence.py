f0ba4b36eac9a54281d46ef12419bf9f5949c8ad
Aiswarya2316 <aiswaryapinkey@gmail.com>
2024-07-16 10:48:41 +0530

    done

--- oop/inheritence/task_inheritence.py
+++ oop/inheritence/task_inheritence.py
@@ -69,39 +69,127 @@
 
 
 
-            #multilevel inheritence
-class synnefo:
-    def __init__(s):
-        print('register')
-    def python(s):
-        s.database='mysql'
-        print("python")
-    def php(self):
-        self.database='mysql'
-        print("php",self.database)
+                 #multilevel inheritence
 
-class novavi(synnefo):
-    def dm(self):
-        print("digital marketing")
-    def web_dev(self):
-        print("web developer")
+# class college:
+#     def __init__(s):
+#          print('register')
+#     def python(s):
+#          print("python")
+#     def php(self):
+#         print("php")
+#     def english(self):
+#         print("fine tune") 
+#     def accounting(self):
+#         print("bank accounting") 
+#     def span(self):
+#         print("spanish") 
 
-class staff(novavi):
-    def reg(self):
-        print('std reg')
+# class teachers(college):
+#     def web_dev(self):
+#         print("web developer")
+#     def maths(self):
+#         print("mathematics") 
 
-anu=synnefo()
-anu.php()
+# class staff(teachers):
+#     def reg(self):
+#         print('staff reg')
 
-achu=novavi()
-achu.dm()
-achu.php
+# anu=college()
+# anu.php()
 
-anju=staff()
-anju.python()
-anju.reg
+# achu=teachers()
+# achu.web_dev()
+# achu.php()
+
+# anju=staff()
+# anju.python()
+# anju.reg
 
          
 
            
-           
+
+                        #hybrid        
+
+# class college:
+#     def __init__(s):
+#          print('register')
+#     def python(s):
+#          print("python")
+#     def php(self):
+#         print("php")
+#     def english(self):
+#         print("fine tune") 
+#     def accounting(self):
+#         print("bank accounting") 
+#     def span(self):
+#         print("spanish") 
+
+# class teachers(college):
+#     def web_dev(self):
+#         print("web developer")
+#     def maths(self):
+#         print("mathematics")
+
+# class std(teachers):
+#     def reg(self):
+#         print('std reg')
+
+# class staff(college,teachers):
+#     def wrk(self):
+#         print("staff reg")
+
+# anu=college()
+# anu.php()
+
+# achu=teachers()
+# achu.accounting()
+
+# anju=std()
+# anju.python()
+# anju.reg
+
+# ammu=staff()
+# ammu.python()
+# ammu.web_dev()
+# ammu.wrk()  
+
+
+
+
+
+                     #heirarchichal
+
+# class college:
+#     def __init__(s):
+#          print('register')
+#     def python(s):
+#          print("python")
+#     def php(self):
+#         print("php")
+#     def english(self):
+#         print("fine tune") 
+#     def accounting(self):
+#         print("bank accounting") 
+#     def span(self):
+#         print("spanish") 
+# class teachers(college):
+#     def dm(self):
+#         print("digital marketing")
+#     def web_dev(self):
+#         print("web developer")
+
+# class std(college):
+#     def reg(self):
+#         print('std reg')
+
+# anu=college()
+# anu.php()
+
+# achu=teachers()
+# achu.dm()
+
+# anju=std()
+# anju.python()
+# anju.reg
