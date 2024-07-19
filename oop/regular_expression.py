55fa86ac183bb33794ff230e7746b417317bf3cb
Aiwarya2316 <aiswaryapinkey@gmail.com>
2024-07-19 11:15:10 +0530

    done

---
+++ oop/regular_expression.py
@@ -0,0 +1,26 @@
+a="abcde"
+import re
+# print(re.sub("w","s",a))
+
+# print(re.split("e",a))
+
+# print(re.sub("welcome","python",a))
+
+# print(re.search("z",a))
+
+# if re.search("l",a):
+#     print("yes")
+# else:
+#     print("nop")            
+
+# print(re.search('[a-m]',a) )  
+
+print(re.search("a.",a))
+
+print(re.search('b..',a))
+
+print(re.search('b.*',a))
+
+print(re.search('b.+',a))
+
+print(re.search('b.?',a))
