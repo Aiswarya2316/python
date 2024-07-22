89fcdc9e5e62eb9fa1ec2fba7b90c2d5cbf40d80
Aiswarya2316 <aiswaryapinkey@gmail.com>
2024-07-22 11:26:13 +0530

    exam

--- oop/regular_expression.py
+++ oop/regular_expression.py
@@ -15,24 +15,32 @@ import re
 
 # print(re.search('[a-m]',a) )  
 
-a="abcde"
+# a="abcde"
 
-print(re.search("a.",a))
+# print(re.search("a.",a))
 
-print(re.search('b..',a))
+# print(re.search('b..',a))
 
-print(re.search('b.*',a))
+# print(re.search('b.*',a))
 
-print(re.search('b.+',a))
+# print(re.search('b.+',a))
 
-print(re.search('b.?',a))
+# print(re.search('b.?',a))
 
-print(re.search('[a-z].',a) )  
+# print(re.search('[a-z].',a) )  
 
-print(re.search('[a-z].+',a) )
+# print(re.search('[a-z].+',a) )
 
-print(re.search('[a-z].*',a) )  
+# print(re.search('[a-z].*',a) )  
 
-print(re.search('[A-Z].?',a) )  
+# print(re.search('[A-Z].?',a) )  
 
-print(re.search('[a-z]..',a) )  
+# print(re.search('[a-z]..',a) )  
+
+a="AabcBd123456"
+print(re.search('[a-z]',a))
+
+print(re.search('[a-z][0-9]',a) )
+
+print(re.search('[A-Z][a-z][0-9]',a))
+
