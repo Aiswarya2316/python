4b6eec3d9d30a124be860d9f73146d946ea9c725
Aiswarya2316 <aiswaryapinkey@gmail.com>
2024-10-15 09:38:03 +0530

    exm

--- exm.py
+++ exm.py
@@ -1,35 +1,37 @@
-# n = 3  # Number of rows
+# n = 3  
 
 # for i in range(n):
-#     # Inner loop to print characters in reverse order
+
 #     for j in range(i, -1, -1):
 #         print(chr(65 + j), end=" ")
-#     print()  # New line after each row
+#     print()  
 
 
 
 
-# n = 3  # Number of rows
+n=65 
+
+for i in range(3):
+    a=1
+    for j in range(3):
+        if i == j:
+            print(a, end=" ")
+        else:
+            print(chr(n), end=" ")
+    print() 
+    n+=1 
+
+
+
+
+
+# n = 3  
 
 # for i in range(n):
-#     for j in range(n):
-#         if i == j:
-#             print(1, end=" ")
-#         else:
+#     if i % 2 == 0:  
+#         for j in range(n):
 #             print(chr(65 + j), end=" ")
-#     print()  # New line after each row
-
-
-
-
-
-n = 3  # Number of rows
-
-for i in range(n):
-    if i % 2 == 0:  # For even rows (0, 2)
-        for j in range(n):
-            print(chr(65 + j), end=" ")
-    else:  # For odd row (1)
-        for j in range(n - 1, -1, -1):
-            print(chr(65 + j), end=" ")
-    print()  # Move to
+#     else:  
+#         for j in range(n - 1, -1, -1):
+#             print(chr(65 + j), end=" ")
+#     print()  
